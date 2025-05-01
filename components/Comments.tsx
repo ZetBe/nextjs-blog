ceeb3cb6069155574e54ccccb548fb2c067ab974
ZetBe <seoheewon123@hufs.ac.kr>
2025-05-01 13:15:33 +0900

    refactor: 에러 개선

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -4,5 +4,8 @@ import { Comments as CommentsComponent } from 'pliny/comments'
 import siteMetadata from '@/data/siteMetadata'
 
 export default function Comments({ slug }: { slug: string }) {
+  if (!siteMetadata.comments) {
+    return null
+  }
   return <CommentsComponent commentsConfig={siteMetadata.comments} slug={slug} />
 }
