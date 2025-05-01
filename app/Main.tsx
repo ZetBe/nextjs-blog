78ffa2d78e2e693c75bf9efeec274bb37bca6999
ZetBe <seoheewon123@hufs.ac.kr>
2025-05-01 12:45:58 +0900

    chore: 불필요한 부분 제거

--- app/Main.tsx
+++ app/Main.tsx
@@ -10,10 +10,10 @@ export default function Home({ posts }) {
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pt-12 pb-8 md:space-y-5">
-          <h2 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
-            함 둘러보쇼
-          </h2>
+        <div className="space-y-6 pt-12 pb-8 md:space-y-5">
+          <h3 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
+            둘러보기
+          </h3>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
           </p>
@@ -79,11 +79,6 @@ export default function Home({ posts }) {
           >
             All Posts &rarr;
           </Link>
-        </div>
-      )}
-      {siteMetadata.newsletter?.provider && (
-        <div className="flex items-center justify-center pt-4">
-          <NewsletterForm />
         </div>
       )}
     </>
